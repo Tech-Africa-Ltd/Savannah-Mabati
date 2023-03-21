c319e5535e9d787ca9c60534f84b2b9050cd54ef
kajela <titoyut56@gmail.com>
2023-03-21 02:34:31 -0400

    add the initial files

---
+++ gas/forms.py
@@ -0,0 +1,42 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import Catalogue, Profile
+
+
+class UserRegistration(forms.ModelForm):    
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label='Repeat Password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', )
+
+        def clean_password2(self):
+            cd = self.cleaned_data
+            if cd['password'] != cd['password2']:
+                raise forms.ValidationError('Passwords don\'t match.')
+            return cd['password2']
+
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
+
+class CatalogueForm(forms.ModelForm):
+    class Meta:
+        model = Catalogue
+        exclude = ["dateAdded"]
+        fields = ['image','price','name', 'availability']
+
+class ProfileForm(forms.ModelForm):
+    firstName = forms.CharField(max_length=30)
+    lastName = forms.CharField(max_length=30)
+    email = forms.CharField(max_length=30)
+
+    class Meta:
+        # dp = ImageField()
+        model = Profile
+        fields = ['firstName', 'lastName', 'email', 'bio']
---
+++ gas/models.py
@@ -0,0 +1,90 @@
+from distutils.command.upload import upload
+from this import d
+from django.conf import settings
+from django.db import models
+from django.contrib.auth.models import User
+import datetime as dt
+from datetime import datetime
+# Create your models here.
+
+
+class UserRegistrationModel(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+class Profile(models.Model):
+    phone_number = models.CharField(max_length=15)
+    user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
+    # dp = Ime(blank=True, manual_crop="")
+    bio = models.TextField(max_length=500)
+    email_confirmed = models.BooleanField(default=False)    
+
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    def __str__(self):
+        return self.user.username
+
+
+class Catalogue(models.Model):
+    categories = (
+        ('Phone', 'Phone'),
+        ('Tablet', 'Tablet'),
+        ('Laptop', 'Laptop'),
+        ('Television', 'Television'),
+        ('Printer', 'Printer'),
+        ('Desktop', 'Desktop'),
+        ('Watch', 'Watch'),
+    )
+    image = models.ImageField(upload_to="images/")
+    price = models.IntegerField()
+    name = models.CharField(max_length=30)            
+    availability = models.BooleanField(default=False)
+    dateAdded = models.DateField(default=datetime.utcnow)
+
+
+    def __str__(self):
+        return self.name
+
+    @classmethod
+    def get_single_product(cls, pk):
+        product = cls.objects.get(pk=pk)
+        return product
+
+class Transactions(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='transactions')
+    dateAdded = models.DateField(default=datetime.utcnow)
+    receipt = models.CharField(max_length=50, null=True)
+    amount = models.FloatField()
+    phoneNumber = models.CharField(max_length=15)
+    checkoutReuestID = models.CharField(max_length=50)
+    merchantRequestId = models.CharField(max_length=50)
+    status = models.CharField(max_length=50)
+
+
+    def __str__(self):
+        return self.status
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cartItems')
+    catalogue = models.ForeignKey(Catalogue, on_delete=models.CASCADE, related_name='cartItems')
+    quantity = models.IntegerField(default=1)
+
+
+    def __str__(self):
+        return self.catalogue
+
+
+class Orders(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
+    catalogue = models.ForeignKey(Catalogue, on_delete=models.CASCADE, related_name='orders')
+    transaction = models.ForeignKey(Transactions, on_delete=models.CASCADE, related_name='orders')
+    isPaid = models.BooleanField(default=False)
+    isDelivered = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.catalogue
